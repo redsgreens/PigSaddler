f22b81b728888289ac4352044409aaf277733305
Mike <redsgreens@gmail.com>
2011-05-12 14:04:29 -0700

    multiworld and percentages

--- src/redsgreens/PigSaddler/PigListener.java
+++ src/redsgreens/PigSaddler/PigListener.java
@@ -1,11 +1,18 @@
 package redsgreens.PigSaddler;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import org.bukkit.entity.Pig;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityListener;
 
 public class PigListener extends EntityListener {
 	PigSaddler Plugin;
+	Random rand = new Random();
+
+	// list of pigs that need saddles
+	static ArrayList<Pig> Pigz = new ArrayList<Pig>();
 	
 	public PigListener(PigSaddler plugin) {
 		Plugin = plugin;
@@ -14,16 +21,21 @@ public class PigListener extends EntityListener {
 	public void onCreatureSpawn(CreatureSpawnEvent event) {
 		if(event.isCancelled()) return;
 		
-//		if(event.getEntity() instanceof Pig && event.getLocation().getWorld().getName().equalsIgnoreCase("world")) {
-		if(event.getEntity() instanceof Pig) {
+		if(event.getEntity() instanceof Pig) 
+		{
 			Pig pig = (Pig) event.getEntity();
-			PigSaddler.Pigz.add(pig);
+			Pigz.add(pig);
 			Plugin.getServer().getScheduler().scheduleSyncDelayedTask(Plugin, new Runnable() {
 			    public void run() {
-			    	while(PigSaddler.Pigz.size() > 0){
-			    		Pig pig = PigSaddler.Pigz.get(0);
-			    		pig.setSaddle(true);
-			    		PigSaddler.Pigz.remove(0);
+			    	Integer worldPercent;
+			    	while(Pigz.size() > 0){
+			    		Pig pig = Pigz.get(0);
+			    		worldPercent = PigSaddler.getWorldSaddlePercent(pig.getLocation().getWorld().getName());
+			    		if(worldPercent != 0)
+				    		if(rand.nextInt((Integer)(100 / worldPercent)) == 0)
+				    			pig.setSaddle(true);
+
+			    		Pigz.remove(0);
 			    	}
 			    }
 			}, 0);
--- src/redsgreens/PigSaddler/PigSaddler.java
+++ src/redsgreens/PigSaddler/PigSaddler.java
@@ -1,35 +1,97 @@
 package redsgreens.PigSaddler;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStream;
 import java.util.HashMap;
 
-import org.bukkit.entity.Pig;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
+import org.yaml.snakeyaml.Yaml;
 
 /**
  * @author redsgreens
  **/
 public class PigSaddler extends JavaPlugin {
     private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
+	private PluginDescriptionFile pdfFile;
+	private static HashMap<String, Integer> configMap = new HashMap<String, Integer>();
+
     public PigListener pigListener;
 	public PluginManager pluginManager;
 
-	public static ArrayList<Pig> Pigz = new ArrayList<Pig>();
 
     public void onEnable() {
     	pluginManager = getServer().getPluginManager();
+
+    	loadConfig();
+    	
     	pigListener = new PigListener(this);
         pluginManager.registerEvent(Event.Type.CREATURE_SPAWN, pigListener, Priority.Normal, this);
 
-        PluginDescriptionFile pdfFile = this.getDescription();
+        pdfFile = this.getDescription();
         System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
     }
-    public void onDisable() {}
+    
+    @SuppressWarnings("unchecked")
+	private void loadConfig()
+    {
+    	try
+    	{
+        	File folder = this.getDataFolder();
+        	// create the data folder if it doesn't exist
+        	if(!folder.exists()){
+        		folder.mkdirs();
+        	}
+
+        	File configFile = new File(folder, "config.yml");
+    		// create a default config if one doesn't exist
+        	if (!configFile.exists()){
+    			configFile.createNewFile();
+    			InputStream res = PigSaddler.class.getResourceAsStream("/config.yml");
+    			FileWriter tx = new FileWriter(configFile);
+    			for (int i = 0; (i = res.read()) > 0;) tx.write(i);
+    			tx.flush();
+    			tx.close();
+    			res.close();
+    		}
+
+    		BufferedReader rx = new BufferedReader(new FileReader(configFile));
+    		Yaml yaml = new Yaml();
+    		
+    		// clear the config before loading
+    		configMap.clear();
+
+    		// load it
+    		configMap = (HashMap<String,Integer>)yaml.load(rx);
+    		
+    		// close the file
+    		rx.close();
+
+     	} catch (Exception e) {
+    		System.out.println("PigSaddler error: " + e.getMessage());
+    	}
+
+    }
+    
+    public static Integer getWorldSaddlePercent(String world)
+    {
+    	if(configMap.containsKey(world))
+    		return configMap.get(world);
+    	else
+    		return 0;
+    }
+    
+    public void onDisable() {
+        System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is disabled." );
+    	
+    }
     
     public boolean isDebugging(final Player player) {
         if (debugees.containsKey(player)) {
